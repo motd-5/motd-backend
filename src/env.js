5c6e0f6ed06d54fa0ea33d9500c0396830d1d3ed
Unchaptered <workstation19961002@gmail.com>
2022-07-22 15:32:26 +0900

    Update `src/env.js`, applying Singleton

--- src/env.js
+++ src/env.js
@@ -2,17 +2,33 @@ import dotenv from 'dotenv/config';
 
 /**
  * `Env` class is craeted for single instance.
+ * 
+ * If you don't know Singleton Pattern.
+ * 
+ * Please visit : https://github.com/Boiler-Express/.github/blob/main/notes/design/SINGLETON.md
  */
 export default class Env {
 
-    mode;
-    port;
+    static env;
 
-    constructor() {
+    constructor() {}
 
-        this.mode = process.env.mode;
-        this.port = process.env.port;
+    /**
+     * getEnvInstance should return `Environment Object`.
+     * 
+     * @returns `Object Literal`
+     */
+    static getEnvInstance() {
 
+        if (this.env) return Env.env;
+
+        this.env = {
+            MODE: process.env.NODE_ENV,
+            PORT: process.env.PORT
+        };
+
+        return this.env;
+        
     }
 
 }
