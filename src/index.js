2625c5f69253f922b3b437472f1bffdfbbe023e9
Unchaptered <workstation19961002@gmail.com>
2022-08-13 00:55:42 +0900

    Update `src/index.js`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => console.log(`Server is running on 3000`));
