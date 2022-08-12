da0adbb4210bdeee019e9a954de6f08593fa2263
waveinyu <waveinyu@gmail.com>
2022-08-13 01:22:39 +0900

    morgan install

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.listen(3000, () => console.log(`Server is running on 3000`));
