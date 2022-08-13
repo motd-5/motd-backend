d039246b3864ca703e2a3d141247446a2fe3b9f1
Unchaptered <workstation19961002@gmail.com>
2022-08-14 00:29:15 +0900

    Create `seuelize init`

---
+++ src/sequelize/models/comment.js
@@ -0,0 +1,34 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Comment extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Comment.init(
+        {
+            userId: DataTypes.NUMBER,
+            content: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: 'Comment',
+        },
+    );
+    // https://wooooooak.github.io/node.js/2018/08/22/sequelize1%EB%8C%801/
+    Comment.associate = function (models) {
+        Comment.belongsTo(models.User, {
+            foreignKey: 'userId', // comment.userId
+            targetKey: 'userId', // user.userId
+            onUpdate: 'cascade',
+            onDelete: 'cascade',
+        });
+    };
+    return Comment;
+};
