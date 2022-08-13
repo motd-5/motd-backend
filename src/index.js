d34a4bf75a1b4bb9e894d2b081b799cd86faf1a8
Unchaptered <workstation19961002@gmail.com>
2022-08-13 12:20:32 +0900

    Apply 라우터 연결 in `index.js`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const morgan = require('morgan');
 const express = require('express');
-const { globalRouter, musicRouter } = require('./layers/_.loader');
+const { globalRouter, musicRouter, commentRouter } = require('./layers/_.loader');
 
 const app = express();
 
@@ -10,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('', globalRouter);
 app.use('/musics', musicRouter);
+app.use('/comments', commentRouter);
 
 app.listen(3000, () => console.log(`Server is running on 3000`));
