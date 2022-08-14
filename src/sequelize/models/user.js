445f04df8d56de3ab3660eb146568e302d2927f0
Unchaptered <workstation19961002@gmail.com>
2022-08-15 00:15:55 +0900

    Fix `src/sequelize`, 마이그레이션 및 Model 오작동 수정

--- src/sequelize/models/user.js
+++ src/sequelize/models/user.js
@@ -1,5 +1,12 @@
 'use strict';
 const { Model } = require('sequelize');
+
+/**
+ *
+ * @param {*} sequelize
+ * @param {*} DataTypes
+ * @returns { User }
+ */
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
         /**
@@ -11,11 +18,19 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     }
+    // Error: Unknown column 'id' in 'field list'
+    // https://stackoverflow.com/questions/60152711/why-doesnt-sequelize-cli-include-the-id-in-the-model-file
     User.init(
         {
+            userId: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+            },
             email: DataTypes.STRING,
             nickname: DataTypes.STRING,
             password: DataTypes.STRING,
+            createdAt: DataTypes.DATE,
+            updatedAt: DataTypes.DATE,
         },
         {
             sequelize,
