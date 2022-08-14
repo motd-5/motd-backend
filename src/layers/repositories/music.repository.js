f6445a0cee2e85da23b9366d530b047df5f6b639
waveinyu <waveinyu@gmail.com>
2022-08-15 00:19:36 +0900

    Update `repositories/music.repository.js`

--- src/layers/repositories/music.repository.js
+++ src/layers/repositories/music.repository.js
@@ -1,8 +1,13 @@
+const sequelize = require('sequelize');
+const { Music } = require('../../sequelize/models/music')(sequelize, DataTypes);
+
 class MusicRepository {
     // repository는 mongoose의 findbyid와 비슷 (id 기준으로 단일 문서 조회)
     constructor() {}
 
-    postMusics = () => {
+    postMusics = async (title, artist, album, musicUrl) => {
+        // const post = await Music.create;
+        // console.log('저장 성공!', post);
         return 'hello';
     };
 
