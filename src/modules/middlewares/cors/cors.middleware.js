5eb2b25ba870da80ef3abcaefbc6242c129f4280
Unchaptered <workstation19961002@gmail.com>
2022-08-15 19:13:04 +0900

    Create 기본 Middleware Setup 구성

---
+++ src/modules/middlewares/cors/cors.middleware.js
@@ -0,0 +1,11 @@
+const e = require('express');
+
+/**
+ *
+ * @param { e.Request } req
+ * @param { e.Response } res
+ * @param { e.NextFunction } next
+ */
+const corsMiddleware = (req, res, next) => {};
+
+module.exports = corsMiddleware;
