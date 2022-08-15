1c17e8c169a436dc196d444c2032b9d556582eb1
Unchaptered <workstation19961002@gmail.com>
2022-08-15 11:50:54 +0900

    Update `**/user.service.js`, login 메서드 구현(토큰 미구현 상태)

--- src/layers/services/user.service.js
+++ src/layers/services/user.service.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
 const { User } = require('../../sequelize/models');
-const { UserDto, UserJoinDto, UserLoginDto, ConflictException } = require('../../models/_.loader');
+const {
+    UserDto,
+    UserJoinDto,
+    UserLoginDto,
+    ConflictException,
+    NotFoundException,
+    ForbiddenException,
+} = require('../../models/_.loader');
 const { BcryptProvider } = require('../../modules/_.loader');
 const UserRepository = require('../repositories/user.repository');
 
@@ -23,18 +30,41 @@ class UserService {
             const isExists = await this.userRepository.isExistsUser(userJoinDto.email);
             if (isExists)
                 throw new ConflictException(`${userJoinDto.email} 은 사용 중인 이메일입니다.`);
+            else {
+                userJoinDto.password = await this.bcryptProvider.hashPassword(userJoinDto.password);
 
-            userJoinDto.password = await this.bcryptProvider.hashPassword(userJoinDto.password);
-
-            return await this.userRepository.createUser(userJoinDto);
+                return await this.userRepository.createUser(userJoinDto);
+            }
         } catch (err) {
             throw err;
         }
     };
 
-    /**  @param { UserLoginDto } userLoginDto  @returns { string } */
+    /**
+     *
+     * @param { UserLoginDto } userLoginDto
+     * @returns { Promise<string> } email
+     */
     login = async (userLoginDto) => {
-        return 'hello';
+        try {
+            const findedUser = await this.userRepository.findUserEmailAndPassword(
+                userLoginDto.email,
+            );
+            if (findedUser === null)
+                throw new NotFoundException(`${userLoginDto.email} 은 존재하지 않는 이메일입니다.`);
+            else {
+                const isCorrected = await this.bcryptProvider.isCorrectPassword(
+                    userLoginDto.password,
+                    findedUser.password,
+                );
+                if (!isCorrected)
+                    throw new ForbiddenException(`${userLoginDto.email} 의 비밀번호가 틀렸습니다.`);
+
+                return findedUser.email;
+            }
+        } catch (err) {
+            throw err;
+        }
     };
 }
 
