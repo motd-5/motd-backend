e105bfc0ce33d95aa856fd65eb4a34ae9fe77897
Unchaptered <workstation19961002@gmail.com>
2022-08-15 19:44:36 +0900

    Fix `circular exports`, about jwtProvider

--- src/modules/_.loader.js
+++ src/modules/_.loader.js
@@ -4,16 +4,8 @@ const JoiValidator = require('./validator/joi.validator');
 const JwtProvider = require('./providers/jwt.provider');
 const BcryptProvider = require('./providers/bcrypt.provider');
 
-const corsMiddleware = require('./middlewares/cors/cors.middleware');
-const preventLoginGuard = require('./middlewares/guards/prevent.login.guard');
-const preventUnLoginGuard = require('./middlewares/guards/prevent.unlogin.guard');
-
 module.exports = {
     exceptionHandler,
-
-    corsMiddleware,
-    preventLoginGuard,
-    preventUnLoginGuard,
 
     JoiValidator,
 
