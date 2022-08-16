c97b6e49689ddb00b50d0aa6c2d5b3f616ef61f4
Unchaptered <workstation19961002@gmail.com>
2022-08-16 11:18:09 +0900

    Update `**/global.router.js`, 샘플 미들웨어 연결

--- src/layers/routers/global.router.js
+++ src/layers/routers/global.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const { preventLoginGuard, preventUnLoginGuard } = require('../../middlewares/_.loader');
 const UserController = require('../controllers/user.controller');
 
 const globalRouter = Router();
 const userController = new UserController();
 
-globalRouter.post('/join', userController.join);
-globalRouter.post('/login', userController.login);
+globalRouter.post('/join', preventUnLoginGuard, userController.join);
+globalRouter.post('/login', preventUnLoginGuard, userController.login);
 
 module.exports = globalRouter;
