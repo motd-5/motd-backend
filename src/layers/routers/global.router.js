cd9a00b2bd3b904f0b1679d6da999d75cbef17c1
Unchaptered <workstation19961002@gmail.com>
2022-08-16 15:24:54 +0900

    Update `**/*.router.js`, 토큰 미들웨어 반영

--- src/layers/routers/global.router.js
+++ src/layers/routers/global.router.js
@@ -5,7 +5,7 @@ const UserController = require('../controllers/user.controller');
 const globalRouter = Router();
 const userController = new UserController();
 
-globalRouter.post('/join', preventUnLoginGuard, userController.join);
-globalRouter.post('/login', preventUnLoginGuard, userController.login);
+globalRouter.post('/join', preventLoginGuard, userController.join);
+globalRouter.post('/login', preventLoginGuard, serController.login);
 
 module.exports = globalRouter;
--- src/layers/routers/music.router.js
+++ src/layers/routers/music.router.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { s3Middleware } = require('../../middlewares/_.loader');
+const { preventUnLoginGuard, s3Middleware } = require('../../middlewares/_.loader');
 
 const musicRouter = Router();
 const MusicController = require('../controllers/music.controller');
@@ -7,7 +7,12 @@ const MusicController = require('../controllers/music.controller');
 const musicController = new MusicController();
 
 musicRouter.get('', musicController.getMusics);
-musicRouter.post('', s3Middleware.single('userUploadImage'), musicController.postMusics);
+musicRouter.post(
+    '',
+    preventUnLoginGuard,
+    s3Middleware.single('userUploadImage'),
+    musicController.postMusics,
+);
 musicRouter.get('/:musicId', musicController.getOneMusic);
 
 module.exports = musicRouter;
