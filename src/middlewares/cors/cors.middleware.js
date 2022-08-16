c87f2f9f2b7e4c70ae489a4475c40fae379b1b15
Unchaptered <workstation19961002@gmail.com>
2022-08-16 15:31:32 +0900

    Update `**/cors.middleware.js`, 브라우저 CORS 에러 해결을 위한 미들웨어

--- src/middlewares/cors/cors.middleware.js
+++ src/middlewares/cors/cors.middleware.js
@@ -7,6 +7,8 @@ const e = require('express');
  * @param { e.NextFunction } next
  */
 const corsMiddleware = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     return next();
 };
 
