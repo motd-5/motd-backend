0c429801103d22d43e817aefefe515ba3c60ca7e
Unchaptered <workstation19961002@gmail.com>
2022-08-16 12:20:57 +0900

    Update `**/middlewares/_.loader.js`, add s3Middleware

--- src/middlewares/_.loader.js
+++ src/middlewares/_.loader.js
@@ -1,8 +1,10 @@
+const s3Middleware = require('./s3/s3.middleware');
 const corsMiddleware = require('./cors/cors.middleware');
 const preventLoginGuard = require('./guards/prevent.login.guard');
 const preventUnLoginGuard = require('./guards/prevent.unlogin.guard');
 
 module.exports = {
+    s3Middleware,
     corsMiddleware,
     preventLoginGuard,
     preventUnLoginGuard,
