a0fb080d833724dadaa44ab3e08bf16962e7961a
Unchaptered <workstation19961002@gmail.com>
2022-08-16 10:01:48 +0900

    Update `**/index.js`, cors 미들웨어 설치

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const cors = require('express');
 const morgan = require('morgan');
 const express = require('express');
 
@@ -9,9 +10,10 @@ const { globalRouter, musicRouter, commentRouter } = require('./layers/_.loader'
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', globalRouter);
 app.use('/api/musics', musicRouter);
