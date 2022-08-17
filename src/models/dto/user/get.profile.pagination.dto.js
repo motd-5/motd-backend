1ba386dc1fa491e773751f7c79962559221cb305
Unchaptered <workstation19961002@gmail.com>
2022-08-17 12:37:17 +0900

    Update `**/get.musics.dto.js` and `**/get.profile.pagination.dto.js`, following API 명세서

--- src/models/dto/user/get.profile.pagination.dto.js
+++ src/models/dto/user/get.profile.pagination.dto.js
@@ -1,27 +1,32 @@
 const joi = require('joi');
 const BaseDto = require('../base/base.dto');
 
-class GetMusicsDto extends BaseDto {
+class UserDto extends BaseDto {
+    /** @type { number } */
+    userId;
+
     /** @type  { number } */
     page;
 
     /** @type  { number } */
     pageCount;
 
-    /** @param { { page: number, pageCount: number | undefined } } IUserDto */
-    constructor({ page = 1, pageCount = 3 }) {
+    /** @param { { userId: number, page: number, pageCount: number | undefined } } IUserDto */
+    constructor({ userId, page = 1, pageCount = 3 }) {
         super();
 
+        this.userId = userId;
         this.page = page;
         this.pageCount = pageCount;
     }
 
     getJoiInstance() {
         return {
+            userId: joi.number().required(),
             page: joi.number().required(),
             pageCount: joi.number().required(),
         };
     }
 }
 
-module.exports = GetMusicsDto;
+module.exports = UserDto;
