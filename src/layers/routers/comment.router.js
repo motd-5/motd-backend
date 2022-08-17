6cec68abba45c279fea1d4d99a60bbb063c38759
waveinyu <waveinyu@gmail.com>
2022-08-17 23:48:33 +0900

    Update `comment.router.js`

--- src/layers/routers/comment.router.js
+++ src/layers/routers/comment.router.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 
 const CommentController = require('../controllers/comment.controller');
+const { tokenGuard } = require('../../middlewares/_.loader');
 
 const commentRouter = Router();
 const commentController = new CommentController();
 
 commentRouter.get('', commentController.getComments);
-commentRouter.post('', commentController.postComments);
-commentRouter.put('/:commentId', commentController.updateComments);
-commentRouter.delete('/:comentId', commentController.deleteComments);
+commentRouter.post('', tokenGuard, commentController.postComments);
+commentRouter.put('/:commentId', tokenGuard, commentController.updateComments);
+commentRouter.delete('/:commentId', tokenGuard, commentController.deleteComments);
 
 module.exports = commentRouter;
