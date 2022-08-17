de0c000f5a2caf3fdd817bcaa8f6d53d47c0d02d
Unchaptered <workstation19961002@gmail.com>
2022-08-17 14:47:56 +0900

    Create `**/pagination.dto.js`, 페이지네이션을 위한 DTO 생성

---
+++ src/models/dto/pagination.dto.js
@@ -0,0 +1,32 @@
+const joi = require('joi');
+const BaseDto = require('./base/base.dto');
+
+class PaginationDto extends BaseDto {
+    /** @type { number } */
+    userId;
+
+    /** @type  { number } */
+    page;
+
+    /** @type  { number } */
+    pageCount;
+
+    /** @param { { userId: number, page: number, pageCount: number | undefined } } IUserDto */
+    constructor({ userId, page = 1, pageCount = 3 }) {
+        super();
+
+        this.userId = userId;
+        this.page = page;
+        this.pageCount = pageCount;
+    }
+
+    getJoiInstance() {
+        return {
+            userId: joi.number().required(),
+            page: joi.number().required(),
+            pageCount: joi.number().required(),
+        };
+    }
+}
+
+module.exports = PaginationDto;
