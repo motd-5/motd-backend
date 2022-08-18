86abf8534ad4f462b3e5154e6167c91903525f70
Unchaptered <workstation19961002@gmail.com>
2022-08-18 11:21:45 +0900

    Fix `cors.middleware.js`

--- src/middlewares/cors/cors.middleware.js
+++ src/middlewares/cors/cors.middleware.js
@@ -8,6 +8,7 @@ const e = require('express');
  */
 const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     return next();
 };
